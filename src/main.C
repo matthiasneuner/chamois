fd2de168290f860212b950e5c25583dd4becc5e9
Matthias <matthias.neuner@uibk.ac.at>
2022-01-21 00:45:44 +0100

    clang-format

--- src/main.C
+++ src/main.C
@@ -14,20 +14,20 @@
 #include "AppFactory.h"
 
 // Create a performance log
-PerfLog Moose::perf_log("Chamois");
+PerfLog Moose::perf_log( "Chamois" );
 
 // Begin the main program.
 int
-main(int argc, char * argv[])
+main( int argc, char * argv[] )
 {
   // Initialize MPI, solvers and MOOSE
-  MooseInit init(argc, argv);
+  MooseInit init( argc, argv );
 
   // Register this application's MooseApp and any it depends on
   ChamoisTestApp::registerApps();
 
   // Create an instance of the application and store it in a smart pointer for easy cleanup
-  std::shared_ptr<MooseApp> app = AppFactory::createAppShared("ChamoisTestApp", argc, argv);
+  std::shared_ptr< MooseApp > app = AppFactory::createAppShared( "ChamoisTestApp", argc, argv );
 
   // Execute the application
   app->run();
